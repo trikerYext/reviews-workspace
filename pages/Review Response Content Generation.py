da64e58a88481cf466ce6cb773e5d3dbd236a880
riker-t <teddyriker2@gmail.com>
2023-04-17 22:21:09 -0400

    defaults update

--- pages/Review Response Content Generation.py
+++ pages/Review Response Content Generation.py
@@ -37,8 +37,8 @@ def check_password():
         # Password correct.
         return True
 
-if check_password():
 
+if check_password():
 
     st.header("Review Response Content Generation")
     t1, t2, t3 = st.tabs(
@@ -46,29 +46,29 @@ if check_password():
 
     openai.api_key = st.secrets['openai']['API_KEY']
 
-
     @st.cache_data
     def get_model_list():
         #models_response = openai.Model.list()
 
         # models = [x['id'] for x in models_response['data']]
 
-        models = ['gpt-3.5-turbo', 'text-davinci-003', 'davinci', 'babbage', 'curie', 'ada']
+        models = ['gpt-3.5-turbo', 'text-davinci-003',
+                  'davinci', 'babbage', 'curie', 'ada']
         return models
-
 
     @st.cache_data
     def convert_df(df):
         # IMPORTANT: Cache the conversion to prevent computation on every rerun
         return df.to_csv().encode('utf-8')
 
-
     @st.cache_data
-    def generate_response(review_content, review_reviewer_name, prompt, review_rating, model, max_tokens, temperature):
-        prompt_with_inputs = prompt + f'''
+    def generate_response(review_content, review_reviewer_name, user_prompt, yext_prompt, review_rating, model, max_tokens, temperature):
+        prompt_with_inputs = yext_prompt + f'''
         Author: {review_reviewer_name}
         Content: {review_content}
-        Rating: {review_rating}
+        Rating: {review_rating} 
+        
+        Additional Instructions: {user_prompt}
         '''
 
         if "turbo" in model:
@@ -82,42 +82,43 @@ if check_password():
 
         return response
 
-
     with t1:
-        st.warning('Before demoing, make sure to add in the Business Type to the Model Input below', icon="⚠️")
+        st.warning(
+            'Before demoing, make sure to modify the prompt', icon="⚠️")
 
         with st.expander("Show/Hide Model Inputs"):
-            prompt = st.text_area("Prompt", height=300, value='''
-                You are a good support agent, responding to online reviews for a <TYPE OF BUSINESS>. You always respond in a concise and clear manner. Do not promise any refunds or specific action in response to a review.\n
-                Respond to this review positively, using the following rules:\n
-                Acknowledge the author by name\n
-                Show a value statement\n
-                Address the sentiment of the review\n
-                Add a closing statement\n
-                Response in the same language as the review\n
+            yext_prompt = st.text_area("Yext Prompt", height=300, value='''You are a helpful assistant who suggests responses to online reviews for Awesome Business, an Accountant. When suggesting a response, follow these rules: \n
+- Do not offer any refunds, promises, commitments, or specific actions \n
+- Follow the additional instructions provided below
                 ''')
+            user_prompt = st.text_area("User Instructions", height=300, value='''1. Acknowledge the author by name
+2. Offer a value statement
+3. Address any negative experiences had
+4. Add a closing statement
+5. Limit the response to 3 sentences
+6. If the review is less than 3 stars, offer the review a chance to reach out to Calvin@awesomebusiness.com for more help
+7. Respond as if you were the agent writing the response''')
 
-            temperature = st.slider("Temperature", value=0.7)
+            temperature = st.slider("Temperature", value=0.25)
             max_tokens = st.slider("Max Tokens", value=256)
             model = st.selectbox("Model", options=get_model_list())
 
     with t2:
         with st.form("Form"):
 
-            review_rating = st.number_input("Rating", help='Review Rating', value=3.0,
+            review_rating = st.number_input("Rating", help='Review Rating', value=2.0,
                                             step=0.5, max_value=5.0, min_value=1.0)
             review_reviewer_name = st.text_input(
-                "Reviewer Name", value="Jim Chapelle", help='Reviewer Name')
+                "Reviewer Name", value="Marc Ferrentino", help='Reviewer Name')
             review_content = st.text_area(
-                "Review Content", value="I had a less than positive experience with the wait staff. Although we were the only customers present, our order came out incorrect. The risotto was delicious though.", help='Review Content', height=300)
+                "Review Content", value="I had a terrible experience with this accountant. They were unresponsive and unprofessional throughout the entire process. They made numerous errors in my tax filings and failed to communicate with me about important deadlines. I ended up owing more in taxes than I had expected, and it was all because of their incompetence. I would not recommend this accountant to anyone.", help='Review Content', height=300)
 
             form_submitted = st.form_submit_button("Generate Response")
 
         if form_submitted:
 
             response = generate_response(review_content=review_content, review_reviewer_name=review_reviewer_name,
-                                        review_rating=review_rating, model=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
-
+                                         review_rating=review_rating, model=model, user_prompt=user_prompt, yext_prompt = yext_prompt, max_tokens=max_tokens, temperature=temperature)
 
             st.write(response)
 
@@ -132,7 +133,8 @@ if check_password():
             df = pd.read_csv(uploaded_file)
 
             if len(df.index) > 100:
-                st.write("To limit costs, this tool is currently limited to generating 100 responses. Please submit a file with less reviews")
+                st.write(
+                    "To limit costs, this tool is currently limited to generating 100 responses. Please submit a file with less reviews")
 
             else:
                 with st.form("Bulk Form"):
@@ -149,7 +151,7 @@ if check_password():
                         review_reviewer_name = row['Author Name']
 
                         response = generate_response(review_content=review_content, review_reviewer_name=review_reviewer_name,
-                                                    review_rating=review_rating, model=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
+                                                     review_rating=review_rating, model=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
 
                         output_row = {
                             "Content": review_content,
@@ -160,13 +162,14 @@ if check_password():
                         st.subheader("Review")
                         st.write("Author: " + review_reviewer_name)
                         st.write("Rating: " + str(review_rating))
-                        if isinstance(review_content,str):
+                        if isinstance(review_content, str):
                             st.write("Content: " + review_content)
                         st.subheader("Response")
                         st.caption(response)
-                        #st.write(output_row)
+                        # st.write(output_row)
 
-                        output_df = output_df.append(output_row, ignore_index=True)
+                        output_df = output_df.append(
+                            output_row, ignore_index=True)
 
                     csv = convert_df(output_df)
 
@@ -176,5 +179,3 @@ if check_password():
                         file_name='generated_responses.csv',
                         mime='text/csv',
                     )
-
-                    
